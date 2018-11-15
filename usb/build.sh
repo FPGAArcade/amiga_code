55eedc7ab015226bf837a9716d84cc3723a1f529
Erik Hemming <eriquesnk@gmail.com>
2018-11-15 21:42:16 +0100

    update .vamosrc, build.sh and SCOPTIONS

--- usb/build.sh
+++ usb/build.sh
@@ -5,4 +5,15 @@ set -e
 # check if vamos is installed
 command -v vamos >/dev/null 2>&1 || { echo >&2 "vamos not installed! - see : https://github.com/cnvogelg/amitools/blob/master/doc/vamos.md"; exit 1; }
 
-vamos smake $*
+# create output and temprary dirs
+mkdir -p Devs/USBHardware T
+
+# vamos requires absolute path to current working dir
+ABS_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+VAMOS_CWD="$ABS_PATH/Deneb-Device"
+
+vamos -d $VAMOS_CWD smake $*
+
+if [ "$1x" = "cleanx" ]; then
+	rm -rf Devs T
+fi
