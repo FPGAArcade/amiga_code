10ac9a709a2b8efc7c31f6dc22928307b941f55d
Erik Hemming <eriquesnk@gmail.com>
2021-09-20 22:15:15 +0200

    Convert Travis CI to GitHub Workflow actions (#12)
    * First stab at removing travis.yml
    
    * Install amitools
    
    * upload artifacts
    
    * Cache buildenv
    
    * Use md5 hash
    
    * Update status badge
    
    * use crc32 instead of md5
    
    * Change to sha1
    
    * Hash test
    
    * More hash test
    
    * Upload everything for debug
    
    * Rebuilt AddReplayMem with latest vasmm68k_mot
    
    optimizes 'move.l #$xxxx0000,dN' to 'moveq.l #$xxxx,dN + swap dN'
    
    * Remove debug
    
    * Experimental named artifact upload
    
    * Fix artifact upload
    
    * Fix artifact double extension
    
    * Create release workflow
    
    * Disable release
    
    * Update release.yml
    
    * Release info
    
    * cat RELEASE.md
    
    * cleanup
    
    * Create RELEASE.md
    
    * Fix type in RELEASE.md gen
    
    * Update build.yml
    
    * Update release.yml
    
    * Update build.yml
    
    * remove tags-ignore
    
    * Add tags-ignore
    
    * on branches *
    
    * Include version in RELEASE.md
    
    * Delete .travis.yml
    
    * .gitignore RELEASE.md
    
    * Change build status to point to upstream

--- build_rom.sh
+++ build_rom.sh
@@ -11,3 +11,18 @@ romtool scan replay.rom
 romtool -v build -o poseidon.rom -t ext -s 512 -e 3f80000 -a 3f80000 -r 01.00 -f  poseidon/hid.class poseidon/hub.class poseidon/input.device poseidon/massstorage.class poseidon/poseidon.library poseidon/PsdStackloader
 romtool scan poseidon.rom
 romtool info poseidon.rom
+
+echo -n > RELEASE.md
+echo "Changes:" >> RELEASE.md
+echo "- [??] ???." >> RELEASE.md
+echo "" >> RELEASE.md
+echo -n "REPLAY.ROM " >> RELEASE.md
+romtool info replay.rom | grep rom_rev | awk '{print $2}' >> RELEASE.md
+echo "\`\`\`" >> RELEASE.md
+romtool scan replay.rom >> RELEASE.md
+echo "\`\`\`" >> RELEASE.md
+echo -n "POSEIDON.ROM " >> RELEASE.md
+romtool info poseidon.rom | grep rom_rev | awk '{print $2}' >> RELEASE.md
+echo "\`\`\`" >> RELEASE.md
+romtool scan poseidon.rom >> RELEASE.md
+echo "\`\`\`" >> RELEASE.md
