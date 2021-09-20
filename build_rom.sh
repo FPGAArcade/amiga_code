94df55bf3fd66c1bce0e95c7e80bb84f7b0f2f55
Erik Hemming <eriquesnk@gmail.com>
2021-09-20 21:27:17 +0200

    Create RELEASE.md

--- build_rom.sh
+++ build_rom.sh
@@ -11,3 +11,16 @@ romtool scan replay.rom
 romtool -v build -o poseidon.rom -t ext -s 512 -e 3f80000 -a 3f80000 -r 01.00 -f  poseidon/hid.class poseidon/hub.class poseidon/input.device poseidon/massstorage.class poseidon/poseidon.library poseidon/PsdStackloader
 romtool scan poseidon.rom
 romtool info poseidon.rom
+
+echo -n RELEASE.md
+echo "Changes:" >> RELEASE.md
+echo "- [??] ???." >> RELEASE.md
+echo "" >> RELEASE.md
+echo "REPLAY.ROM" >> RELEASE.md
+echo "\`\`\`" >> RELEASE.md
+romtool scan replay.rom >> RELEASE.md
+echo "\`\`\`" >> RELEASE.md
+echo "POSEIDON.ROM" >> RELEASE.md
+echo "\`\`\`" >> RELEASE.md
+romtool scan poseidon.rom >> RELEASE.md
+echo "\`\`\`" >> RELEASE.md
